afbd95c5c666ef3b193a17473ce735573841847f
Kazbek <sh.kazbek0603@gmail.com>
2019-06-25 18:20:31 +0600

    first commit

---
+++ js/common.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+
+    $('.call-menu').on('click', function () {
+        $('.header-bottom').addClass('nav-show');
+        $('.overlay').addClass('showed');
+    });
+
+    $('.close-menu').on('click', function () {
+        $('.header-bottom').removeClass('nav-show');
+        $('.overlay').removeClass('showed');
+    });
+    wow = new WOW({
+        animateClass: 'animated',
+        offset: 100
+    });
+    wow.init();
+    $('#mainNav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 950,
+        scrollThreshold: 0.2,
+        filter: '',
+        easing: 'swing',
+        begin: function() {
+        },
+        end: function() {
+            if (!$('#main-nav ul li:first-child').hasClass('active')) {
+                $('.header').addClass('addBg');
+            } else {
+                $('.header').removeClass('addBg');
+            }
+
+        },
+        scrollChange: function($currentListItem) {
+            if (!$('#main-nav ul li:first-child').hasClass('active')) {
+                $('.header').addClass('addBg');
+            } else {
+                $('.header').removeClass('addBg');
+            }
+        }
+    });
+
+    $('.call-callback').on('click', function () {
+        $('.callback').addClass('showed');
+        $('.overlay').addClass('showed');
+    });
+    $('.close-callback').on('click', function () {
+        $('.callback').removeClass('showed');
+        $('.overlay').removeClass('showed');
+    });
+    $('.overlay').on('click', function () {
+        $('.callback').removeClass('showed');
+        $('.overlay').removeClass('showed');
+    });
+
+});
+
+
+
+
